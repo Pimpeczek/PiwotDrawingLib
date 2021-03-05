23ad87a8b5d016ef36d374e2ea59125adbc5031e
Piotr Wątorski <piotr.watorski@gmail.com>
2021-03-05 01:06:23 +0100

    Syncer test1

---
+++ PiwotDrawingLib/Drawing/Syncable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiwotDrawingLib.Drawing
+{
+    public abstract class Syncable
+    {
+        public abstract bool IsOutdated { get; protected set; }
+        public abstract void Update();
+        public abstract void OnRegister();
+        public abstract void OnUnRegister();
+        public abstract void Unregister();
+        public abstract void Register();
+    }
+}
---
+++ PiwotDrawingLib/Drawing/Synchronizer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PiwotDrawingLib.Drawing
+{
+    public static class Synchronizer
+    {
+        private static List<Syncable> Syncables;
+        private static Thread syncThread;
+        public static int frameLength = 60;
+        public static bool Alive;
+        public static bool Working;
+        public static bool IsReady { get; private set; }
+        static Synchronizer()
+        {
+            Alive = true;
+            Working = true;
+            syncThread = new Thread(UpdatingLoop);
+            syncThread.Start();
+        }
+        //RENDERER TO MOŻE SPOKOJNIE ROBIĆ, trzeba to tam dać i podzielić czas klatki na synchronizację i renderowanie.
+        private static void UpdatingLoop()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            while(Alive)
+            {
+                while(Working)
+                {
+                    IsReady = false;
+                    stopwatch.Restart();
+                    Update();
+                    IsReady = true;
+                    if (stopwatch.ElapsedMilliseconds < frameLength)
+                    {
+                        Thread.Sleep(frameLength - (int)stopwatch.ElapsedMilliseconds);
+                    }
+                }
+            }
+        }
+
+        public static void Register(Syncable syncable)
+        {
+            if (!Syncables.Contains(syncable))
+            {
+                Syncables.Add(syncable);
+                syncable.OnRegister();
+            }
+        }
+
+        public static void UnRegister(Syncable syncable)
+        {
+            if (Syncables.Contains(syncable))
+            {
+                Syncables.Remove(syncable);
+                syncable.OnUnRegister();
+            }
+        }
+
+        public static void Update()
+        {
+            foreach(var s in Syncables)
+            {
+                if(s.IsOutdated)
+                    s.Update();
+            }
+        }
+    }
+}
